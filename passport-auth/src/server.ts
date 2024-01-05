2a594279b0d668fec57362e2cb46e66079331c87
SAHA <pkomom123@naver.com>
2024-01-05 20:45:19 +0900

    몽고디비 연결

--- passport-auth/src/server.ts
+++ passport-auth/src/server.ts
@@ -1,7 +1,19 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 const app = express();
 const port = 4000;
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URI!)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.log('db connect error', err);
+  });
 
 app.use(express.json());
 // 폼 데이터를 받기 위한 설정
