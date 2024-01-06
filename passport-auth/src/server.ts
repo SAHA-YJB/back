e66f31a2c6c820eea7ceb0150ec3d2cc3193a7e4
SAHA <pkomom123@naver.com>
2024-01-06 20:36:38 +0900

    ejs로 간단한 로그인화면 만들기

--- passport-auth/src/server.ts
+++ passport-auth/src/server.ts
@@ -16,11 +16,26 @@ mongoose
     console.log('db connect error', err);
   });
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 app.use(express.json());
 // 폼 데이터를 받기 위한 설정
 app.use(express.urlencoded({ extended: false }));
 // 정적 파일을 제공하기 위한 설정
 app.use('/static', express.static(path.join(__dirname, 'public')));
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.post('/login', (req, res) => {});
+
+app.get('/signup', (req, res) => {
+  res.render('signup');
+});
+
+app.post('/signup', (req, res) => {});
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
